dc61bc10bf94596270a7979f95bd52489859ddc0
unfselo-dot <unf.selo@gmail.com>
2026-03-28 18:41:29 +0200

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -2,8 +2,8 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 def game_keyboard(game_id: int):
     kb = InlineKeyboardBuilder()
-    kb.button(text="✅ Записаться", callback_data=f"join:{game_id}")
-    kb.button(text="❌ Отменить", callback_data=f"leave:{game_id}")
-    kb.button(text="💸 Я оплатил", callback_data=f"paid:{game_id}")
+    kb.button(text="✅ Записатися", callback_data=f"join:{game_id}")
+    kb.button(text="❌ Скасувати", callback_data=f"leave:{game_id}")
+    kb.button(text="💸 Я оплатив", callback_data=f"paid:{game_id}")
     kb.adjust(1)
     return kb.as_markup()
